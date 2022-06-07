3a1e3471037aa0ede6b92bdeba857d02f9d39fd6
Joao Pinto <joaomdiaspinto@gmail.com>
2022-06-07 21:31:24 +0100

    First Class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+	
+	
+	public void setValues(int row, int column) {
+		this.row=row;
+		this.column=column;
+	}
+	
+	@Override 
+	public String toString() {
+		return row + ", " + column;
+	}
+
+}
